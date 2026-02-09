4b5aa6f0b5364f4dc4ad650f1b0c65cde0676ec8
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2026-02-08 23:47:37 -0500

    replace oil.nvim with yazi.nvim, disable inlay hints, enable soft wrap in helix

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -67,8 +67,8 @@ end, { desc = "Clear search and close floats" })
 
 map("n", "<C-w>v", function()
   vim.cmd("vsplit")
-  require("oil").open(vim.fn.expand("#:p:h"))
-end, { desc = "Vertical split with Oil (file dir)" })
+  require("yazi").yazi()
+end, { desc = "Vertical split with Yazi" })
 
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -1,17 +1,12 @@
 return {
   {
-    "stevearc/oil.nvim",
-    lazy = false,
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
     keys = {
-      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+      { "-", "<cmd>Yazi<cr>", desc = "Open yazi at current file" },
     },
     opts = {
-      default_file_explorer = true,
-      skip_confirm_for_simple_edits = true,
-      watch_for_changes = true,
-      view_options = {
-        show_hidden = true,
-      },
+      open_for_directories = true,
     },
   },
 }
